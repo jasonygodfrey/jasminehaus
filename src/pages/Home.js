9357eb95a4ace9bfb17ea3ad95097fa6d0f20162
Jason <jasonygodfrey@gmail.com>
2023-05-16 01:08:20 -0700

    2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
         <Fragment>
             <MetaTags>
                 <meta charSet="UTF-8" />
-                <title>Home | Oxer - Minimal Portfolio React Template</title>
+                <title>Home | Jasmine Haus - Phillipino Non-Profit</title>
 
                 <meta httpEquiv="x-ua-compatible" content="ie=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
