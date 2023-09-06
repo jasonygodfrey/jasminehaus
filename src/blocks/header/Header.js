7b5bbed4825277b38f3515c8e52ff63018fb35ed
Jason <jasonygodfrey@gmail.com>
2023-09-05 21:10:38 -0700

    dsfdsfdfs

--- src/blocks/header/Header.js
+++ src/blocks/header/Header.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Logo from '../../blocks/logo/Logo';
 import SearchModal from '../../components/modal/SearchModal';
 import MenuModal from '../../components/modal/MenuModal';
+import jlogo from '../../assets/img/logo/jlogo.png';  // adjust the relative path as needed
+
+// ... later in your code
+<Logo image={jlogo} />
+
 
 const Header = () => {
     return (
         <header id="header" className="site-header">
             <div className="wrapper d-flex justify-content-between">
                 <div className="align-self-center">
-                    <Logo image={ "/assets/img/logo/logo.svg" } />
+                <Logo image={jlogo} style={{ width: '400px', height: '200px' }} />
+
                 </div>
 
                 
